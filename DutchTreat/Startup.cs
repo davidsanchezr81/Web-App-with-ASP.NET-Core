c7cd1bc8b97f7099c17dfc6736029ba5f04f531d
David Sanchez <d.sanchez-rodriguez@craneware.com>
2019-11-17 15:31:14 +0000

    another commit

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,26 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg => { cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString")); });
             services.AddTransient<IMailServices, NullMailServices>();
             services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsEnvironment("Development"))
@@ -41,10 +44,15 @@ namespace DutchTreat
                 cfg.MapRoute(
                     name: "Default",
                     template: "/{controller}/{action}/{id?}",
-                    defaults:new { controller = "App", Action = "Index" });
-                });
+                    defaults: new { controller = "App", Action = "Index" });
+            });
 
 
         }
     }
 }
+
+    
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
